869eb39066f6a353007e3f6192c9da0cb81b8a50
Joseph Thomas Mansilla <jomansilla@frba.utn.edu.ar>
2026-04-16 15:18:55 -0300

    endpoints

--- index.js
+++ index.js
@@ -21,22 +21,33 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
 
-app.get(`${PATH_MEDICAL_V1}/disponibilidadMedico`, medicoController.verDisponibilidad);
-app.post(`${PATH_MEDICAL_V1}/medico`, medicoController.crearMedico);
-app.put(`${PATH_MEDICAL_V1}/medico`, medicoController.modificarMedico);
-app.delete(`${PATH_MEDICAL_V1}/medico`, medicoController.eliminarMedico);
+const PATH_DISPONIBILIDAD = PATH_MEDICAL_V1 + "disponibilidadMedico";
+const PATH_MEDICO = PATH_MEDICAL_V1 + "medico";
+const PATH_SEDE = PATH_MEDICAL_V1 + "sede";
+const PATH_PRACTICA = PATH_MEDICAL_V1 + "practica";
+const PATH_ESPECIALIDAD = PATH_MEDICAL_V1 + "especialidad";
+const PATH_TURNO = PATH_MEDICAL_V1 + "turno";
 
-app.post(`${PATH_MEDICAL_V1}/sede`, sedeController.crearSede);
-app.put(`${PATH_MEDICAL_V1}/sede`, sedeController.modificarSede);
-app.delete(`${PATH_MEDICAL_V1}/sede`, sedeController.eliminarSede);
+// TODO: query params: ?medico/1+?sede/medrano+?obraSocial/up
 
-app.post(`${PATH_MEDICAL_V1}/practica`, practicaController.crearPractica);
-app.put(`${PATH_MEDICAL_V1}/practica`, practicaController.modificarPractica);
-app.delete(`${PATH_MEDICAL_V1}/practica`, practicaController.eliminarPractica);
+app.get(PATH_DISPONIBILIDAD, medicoController.verDisponibilidades);
+app.get(PATH_DISPONIBILIDAD + "/:id", medicoController.verDisponibilidad);
 
-app.post(`${PATH_MEDICAL_V1}/especialidad`, especialidadController.crearEspecialidad);
-app.put(`${PATH_MEDICAL_V1}/especialidad`, especialidadController.modificarEspecialidad);
-app.delete(`${PATH_MEDICAL_V1}/especialidad`, especialidadController.eliminarEspecialidad);
+app.post(PATH_MEDICO, medicoController.crearMedico);
+app.put(PATH_MEDICO + "/:id", medicoController.modificarMedico);
+app.delete(PATH_MEDICO + "/:id", medicoController.eliminarMedico);
+
+app.post(PATH_SEDE, sedeController.crearSede);
+app.put(PATH_SEDE + "/:id", sedeController.modificarSede);
+app.delete(PATH_SEDE + "/:id", sedeController.eliminarSede);
+
+app.post(PATH_PRACTICA, practicaController.crearPractica);
+app.put(PATH_PRACTICA + "/:id", practicaController.modificarPractica);
+app.delete(PATH_PRACTICA + "/:id", practicaController.eliminarPractica);
+
+app.post(PATH_ESPECIALIDAD, especialidadController.crearEspecialidad);
+app.put(PATH_ESPECIALIDAD + "/:id", especialidadController.modificarEspecialidad);
+app.delete(PATH_ESPECIALIDAD + "/:id", especialidadController.eliminarEspecialidad);
 
 
 app.post(`${PATH_MEDICAL_V1}/disponibilidad`, disponibilidadController.crearDisponibilidad);
@@ -44,5 +55,5 @@ app.delete(`${PATH_MEDICAL_V1}/bajaDisponibilidad`, disponibilidadController.baj
 // TODO: Sería un delete, pero si no queremos borrar de una la disponbilidad horaria, sino marcar
 // TODO: como aBorrar y luego borrar con algo interno de nuestro back. Podría ser un patch para ese caso
 
-app.post(`${PATH_MEDICAL_V1}/turno`, turnoController.crearTurno);
-app.delete(`${PATH_MEDICAL_V1}/turno`, turnoController.bajaTurno);
+app.post(PATH_TURNO, turnoController.crearTurno);
+app.delete(PATH_TURNO + "/:id", turnoController.bajaTurno);
