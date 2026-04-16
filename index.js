1476993aa50732e0dbf80df9d9edd2a64a03d269
Joseph Thomas Mansilla <jomansilla@frba.utn.edu.ar>
2026-04-16 04:26:45 -0300

    dependencias e index listo para completar

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ import express from 'express'
 import swaggerUi from 'swagger-ui-express'
 import { readFileSync } from 'fs'
 import { parse } from 'yaml'
+import { z } from "zod";
 
 const app = express();
 const port = 20000;
 const spec = parse(readFileSync("./docs.yaml", "utf-8"));
+const PATH_MEDICAL_V1 = "/v1/medical";
 
 app.use(bodyParser.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spec));
