5981d6c5cfa0ffbe1c1c57887937fa05abe07b7d
Joseph Thomas Mansilla <jomansilla@frba.utn.edu.ar>
2026-04-16 14:42:25 -0300

    muchos crud

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ import { z } from "zod";
 const app = express();
 const port = 20000;
 const spec = parse(readFileSync("./docs.yaml", "utf-8"));
-const PATH_MEDICAL_V1 = "/v1/medical";
+const PATH_MEDICAL_V1 = "/v1/sweet_medical";
 
 app.use(bodyParser.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spec));
@@ -21,5 +21,28 @@ app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 });
 
-smsldsdkes un crack :)
-jetbrains
+app.get(`${PATH_MEDICAL_V1}/disponibilidadMedico`, medicoController.verDisponibilidad);
+app.post(`${PATH_MEDICAL_V1}/medico`, medicoController.crearMedico);
+app.put(`${PATH_MEDICAL_V1}/medico`, medicoController.modificarMedico);
+app.delete(`${PATH_MEDICAL_V1}/medico`, medicoController.eliminarMedico);
+
+app.post(`${PATH_MEDICAL_V1}/sede`, sedeController.crearSede);
+app.put(`${PATH_MEDICAL_V1}/sede`, sedeController.modificarSede);
+app.delete(`${PATH_MEDICAL_V1}/sede`, sedeController.eliminarSede);
+
+app.post(`${PATH_MEDICAL_V1}/practica`, practicaController.crearPractica);
+app.put(`${PATH_MEDICAL_V1}/practica`, practicaController.modificarPractica);
+app.delete(`${PATH_MEDICAL_V1}/practica`, practicaController.eliminarPractica);
+
+app.post(`${PATH_MEDICAL_V1}/especialidad`, especialidadController.crearEspecialidad);
+app.put(`${PATH_MEDICAL_V1}/especialidad`, especialidadController.modificarEspecialidad);
+app.delete(`${PATH_MEDICAL_V1}/especialidad`, especialidadController.eliminarEspecialidad);
+
+
+app.post(`${PATH_MEDICAL_V1}/disponibilidad`, disponibilidadController.crearDisponibilidad);
+app.delete(`${PATH_MEDICAL_V1}/bajaDisponibilidad`, disponibilidadController.bajaDisponibilidad);
+// TODO: Sería un delete, pero si no queremos borrar de una la disponbilidad horaria, sino marcar
+// TODO: como aBorrar y luego borrar con algo interno de nuestro back. Podría ser un patch para ese caso
+
+app.post(`${PATH_MEDICAL_V1}/turno`, turnoController.crearTurno);
+app.delete(`${PATH_MEDICAL_V1}/turno`, turnoController.bajaTurno);
