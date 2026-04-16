ba8effd90f7d970e4af698370c39f44a545f087d
Joseph Thomas Mansilla <jomansilla@frba.utn.edu.ar>
2026-04-16 04:09:30 -0300

    dependencies & healthcheck endpoint

---
+++ index.js
@@ -0,0 +1,20 @@
+import bodyParser from "body-parser";
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import { readFileSync } from 'fs'
+import { parse } from 'yaml'
+
+const app = express();
+const port = 20000;
+const spec = parse(readFileSync("./docs.yaml", "utf-8"));
+
+app.use(bodyParser.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spec));
+
+app.get("/healthCheck", (req, res) => {
+    res.status(200).json({mensaje:"Todo marcha bien!"})
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
