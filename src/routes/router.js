7a11d87012b2487afd210e31e5d8f1e5717f2c8c
Joseph Thomas Mansilla <jomansilla@frba.utn.edu.ar>
2026-04-18 17:49:49 -0300

    routes agregadas

---
+++ src/routes/router.js
@@ -0,0 +1,22 @@
+import bodyParser from "body-parser";
+import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import { readFileSync } from 'fs'
+import { parse } from 'yaml'
+import { z } from "zod";
+
+const app = express();
+const port = 20000;
+const spec = parse(readFileSync("./docs.yaml", "utf-8"));
+const PATH_MEDICAL_V1 = "/v1/sweet_medical";
+
+app.use(bodyParser.json());
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(spec));
+
+app.get("/healthCheck", (req, res) => {
+    res.status(200).json({mensaje:"Todo marcha bien!"})
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
